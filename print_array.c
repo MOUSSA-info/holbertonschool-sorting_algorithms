e53203c7557669f7839ae6579173b4b02574ba33
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-04-08 17:18:35 +0200

    0 task

---
+++ print_array.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_array - Affiche les éléments d'un tableau d'entiers
+ * @array: Le tableau à afficher
+ * @size: Le nombre d'éléments dans le tableau
+ *
+ * Description:
+ * Cette fonction parcourt un tableau d'entiers et affiche chaque élément
+ * séparé par une virgule et un espace (", "). Après avoir affiché tous les
+ * éléments, elle ajoute un retour à la ligne.
+ */
+void print_array(const int *array, size_t size)
+{
+for (size_t i = 0; i < size; i++)
+{
+printf("%d%s", array[i], (i < size - 1) ? ", " : "");
+}
+printf("\n");
+}
