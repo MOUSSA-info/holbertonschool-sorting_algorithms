f8885c1f05fe278c954ce3f65b06fd79015f048a
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-04-12 08:39:01 +0200

    0 task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,49 +1,49 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in
- * ascending order using Bubble Sort
- * @array: The array to be sorted
- * @size: The number of elements in the array
- *
- * Description:
- * This function implements the Bubble Sort algorithm.
- * It repeatedly compares
- * adjacent elements in the array and swaps them
- * if they are in the wrong order.
- * After each swap, it prints the current state of
- * the array using the print_array
- * function. The algorithm stops early if no
- * swaps are made during a pass, which
- * means the array is already sorted.
- */
+* bubble_sort - Sorts an array of integers in
+* ascending order using Bubble Sort
+* @array: The array to be sorted
+* @size: The number of elements in the array
+*
+* Description:
+* This function implements the Bubble Sort algorithm.
+* It repeatedly compares
+* adjacent elements in the array and swaps them
+* if they are in the wrong order.
+* After each swap, it prints the current state of
+* the array using the print_array
+* function. The algorithm stops early if no
+* swaps are made during a pass, which
+* means the array is already sorted.
+*/
 void bubble_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
-	{
-		return;
-	}
+if (!array || size < 2)
+{
+return;
+}
 
-	int swapped;
+int swapped;
 
-	for (size_t i = 0; i < size - 1; i++)
-	{
-		swapped = 0;
-		for (size_t j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				int temp = array[j];
+for (size_t i = 0; i < size - 1; i++)
+{
+swapped = 0;
+for (size_t j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+int temp = array[j];
 
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				swapped = 1;
-				print_array(array, size);
-			}
-		}
-		if (!swapped)
-		{
-			break;
-		}
-	}
+array[j] = array[j + 1];
+array[j + 1] = temp;
+swapped = 1;
+print_array(array, size);
+}
+}
+if (!swapped)
+{
+break;
+}
+}
 }
